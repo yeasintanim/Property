eaa21d043faad4aeee0bd26a8c11d3bbf7d1ef10
Yeasin Tanim <YeasinTanim>
2018-11-02 07:00:24 +0600

    prop

---
+++ PropertyApp/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp
+{
+    class Person
+    {
+        //private string firstName;
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        //private string middleName;
+        //private string lastName;
+
+
+        //public string FirstName
+        //{
+        //    get
+        //    {
+        //        return firstName;
+        //    }
+        //    set { firstName = value; }
+        //}
+
+
+        //public string GetFirstName()
+        //{
+        //    return firstName;
+        //}
+        //public void SetFirstName(string firstName)
+        //{
+        //    if (firstName.Length > 1)
+        //    {
+        //        this.firstName = firstName;
+        //    }
+        //}
+
+
+
+        //public string GetFullName()
+        //{
+        //    return GetFirstName() + " " + middleName + " " + lastName;
+        //}
+    }
+}
---
+++ PropertyApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person aPerson = new Person();
+            
+            //aPerson.SetFirstName("Md.");
+
+            Console.WriteLine(aPerson.FirstName);
+
+            Console.ReadKey();
+        }
+    }
+}
